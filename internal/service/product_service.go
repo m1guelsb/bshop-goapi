6e06165a235e8c0c3c44eb7fe60f2ddaa3fb2754
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2024-02-05 16:09:41 -0300

    [feat] add product service

---
+++ internal/service/product_service.go
@@ -0,0 +1,47 @@
+package service
+
+import (
+	"github.com/m1guelsb/eshop-goapi/internal/database"
+	"github.com/m1guelsb/eshop-goapi/internal/entity"
+)
+
+type ProductService struct {
+	ProductDB database.ProductDB
+}
+
+func NewProductService(productDB database.ProductDB) *ProductService {
+	return &ProductService{ProductDB: productDB}
+}
+
+func (ps *ProductService) GetProducts() ([]*entity.Product, error) {
+	products, err := ps.ProductDB.GetProducts()
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (ps *ProductService) GetProductByID(id string) (*entity.Product, error) {
+	product, err := ps.ProductDB.GetProductByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (ps *ProductService) GetProductByCategoryID(categoryID string) ([]*entity.Product, error) {
+	products, err := ps.ProductDB.GetProductByCategoryID(categoryID)
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (ps *ProductService) CreateProduct(name, description, categoryID, imageURL string, price float64) (*entity.Product, error) {
+	product := entity.NewProduct(name, description, categoryID, imageURL, price)
+	_, err := ps.ProductDB.CreateProduct(product)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
